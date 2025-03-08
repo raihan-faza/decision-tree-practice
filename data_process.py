f8308a7cc65a216aff6b1c02ac14688372f75aba
lahh <raihanfazasabiqr@gmail.com>
2025-03-08 22:09:56 +0700

    adding function to encode data and train the model

--- data_process.py
+++ data_process.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 
+# load dataset
 def load_dataset():
     df = pd.read_csv("drug200.csv")
     return df
@@ -9,12 +11,19 @@ def load_dataset():
 
 # checking info from dataset
 def check_dataset_info(df: pd.DataFrame):
+    # print info
     print(df.info())
+    # print other info
     print(df.describe())
+    # print shape
     print(df.shape)
+    # cheking null value
     print(df.isna().sum())
+    # checking class on all object dtype
+    print(df.select_dtypes(include=["object"]).nunique())
 
 
+# split dataset into train and validation
 def split_dataset(
     df: pd.DataFrame,
     test_size: float,
@@ -22,7 +31,15 @@ def split_dataset(
 ):
     X = df.iloc[:, :-1]  # Select all columns except the last one
     y = df.iloc[:, -1]  # Select only the last column
-    X_train, X_test, y_train, y_test = train_test_split(
+    X_train, X_val, y_train, y_val = train_test_split(
         X, y, test_size=0.2, random_state=1
     )
-    return X_train, X_test, y_train, y_test
+    return X_train, X_val, y_train, y_val
+
+
+def encode_data(X_train, X_test):
+    for col in X_train.select_dtypes(include=["object"]).columns:
+        encoder = LabelEncoder()
+        X_train[col] = encoder.fit_transform(X_train[col])
+        X_test[col] = encoder.fit_transform(X_test[col])
+    return X_train, X_test
--- train.py
+++ train.py
@@ -1,9 +1,15 @@
 import numpy as np
 import pandas as pd
+from sklearn.metrics import classification_report
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 
-from data_process import load_dataset, split_dataset
+from data_process import encode_data, load_dataset, split_dataset
 
 df = load_dataset()
 X_train, X_test, y_train, y_test = split_dataset(df=df, test_size=0.2, random_state=1)
+X_train, X_test = encode_data(X_train, X_test)
 classifier = DecisionTreeClassifier()
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+
+print(classification_report(y_test, y_pred))
